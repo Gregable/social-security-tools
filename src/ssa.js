5e8001e5449bdcf9374ba62ab377b919e5e0ee81
Greg Grothaus <ggrothau@gmail.com>
2017-01-02 01:14:32 -0800

    Remove hack.

--- src/ssa.js
+++ src/ssa.js
@@ -34,7 +34,6 @@ ssaApp.controller("SSAController", function ($scope, $filter, $http, $timeout) {
     $scope.breakPointChart_.setTaxEngine($scope.taxEngine);
     $scope.ageChart_.setTaxEngine($scope.taxEngine);
     $scope.maybeRenderCharts();
-    $scope.loadDemoData(1);
   };
 
   $scope.loadDemoData = function(demoId) {
