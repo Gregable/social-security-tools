985a30ddff97d50fbf5b47847e17079f94f49646
Greg Grothaus <ggrothau@gmail.com>
2017-01-02 00:11:56 -0800

    Initial checkin of all files.

---
+++ src/breakpoint-chart.js
@@ -0,0 +1,383 @@
+/**
+ * Code for driving a breakpoint chart. See partials/breakpoint-chart.html
+ * @constructor
+ */
+function BreakPointChart() {
+  this.canvas_ = null;
+  this.lastRenderedXDollars = -1;
+}
+
+/**
+ * Returns true if the BreakPointChart has been initialized with a canvas
+ * element and taxEngine.
+ * @return {boolean}
+ */
+BreakPointChart.prototype.isInitialized = function() {
+  return this.canvas_ !== null && this.taxEngine !== null;
+};
+
+/**
+ * Sets the canvas on which to render the chart.
+ * @param {Element} canvas_element
+ */
+BreakPointChart.prototype.setCanvas = function(canvas_element) {
+  this.canvas_ = canvas_element;
+  this.context_ = this.canvas_.getContext('2d');
+  // Set the font we will use for labels early so we can measure it's width.
+  this.context_.font = "bold 14px Helvetica"
+  this.mouseToggle = 'ON';
+
+  this.canvas_.addEventListener('mousedown', this.mouseClickListener());
+  this.canvas_.addEventListener('mousemove', this.mouseMoveListener());
+};
+
+/**
+ * Set the taxEngine from which to pull financial data.
+ * @param {TaxEngine} taxEngine
+ */
+BreakPointChart.prototype.setTaxEngine = function(taxEngine) {
+  this.taxEngine_ = taxEngine;
+};
+
+/**
+ * Compute the usable width of the canvas allowable for the chart.
+ * @return {number}
+ */
+BreakPointChart.prototype.chartWidth = function() {
+  // A 6-digit social security payment would be about the highest we
+  // would imagine someone receiving, so we reserve space on the right
+  // to display such a value with a little added padding.
+  var reservedWidth = Math.ceil(
+      this.context_.measureText('$999,999').width) + 10;
+  var usableWidth = this.canvas_.width - reservedWidth;
+  return usableWidth;
+};
+
+/**
+ * Compute the usable height of the canvas allowable for the chart.
+ * @return {number}
+ */
+BreakPointChart.prototype.chartHeight = function() {
+  // A 12pt font is 16 pixels high. We reserve a little extra for padding.
+  var reservedHeight = 16 + 10;
+  var usableHeight = this.canvas_.height - reservedHeight;
+  return usableHeight;
+};
+
+/**
+ * Compute the canvas x-coordinate for a earnings dollars value.
+ * @param {number} earningsX
+ * @return {number}
+ */
+BreakPointChart.prototype.canvasX = function(earningsX) {
+  var xValue = Math.floor(
+      earningsX / this.maxRenderedXDollars() * this.chartWidth());
+  var xClipped = Math.min(xValue, this.chartWidth());
+  return xClipped;
+};
+
+/**
+ * Compute the canvas y-coordinate for a benefit dollars value.
+ * @param {number} benefitY
+ * @return {number}
+ */
+BreakPointChart.prototype.canvasY = function(benefitY) {
+  var yValue = this.chartHeight() -
+      Math.floor(benefitY / this.maxRenderedYDollars() * this.chartHeight());
+  var yClipped = Math.min(yValue, this.chartHeight());
+  return yClipped;
+};
+
+/**
+ * Compute the canvas earnings dollars for an x-coordinate value.
+ * Used for computations involving mouse interactions.
+ * @param {number} canvasX
+ * @return {number}
+ */
+BreakPointChart.prototype.earningsX = function(canvasX) {
+  var xValue = Math.floor(
+      canvasX / this.chartWidth() * this.maxRenderedXDollars());
+  var xClipped = this.maxRenderedXDollars();
+  return Math.min(xValue, xClipped);
+};
+
+/**
+ * Selects a x-coordinate (in dollars) to be the rightmost edge
+ * of our chart.
+ * @return {Number}
+ */
+BreakPointChart.prototype.maxRenderedXDollars = function() {
+  // There are a few goals here when selecting this value:
+  // 1) Show all of the breakpoints so the user can get a feel visually
+  //    for how these breakpoints affect the computation.
+  var breakpoint_min =
+    BENEFIT_BRACKETS[BENEFIT_BRACKETS.length - 1].min * 1.25;
+  // 2) Show the user's current earnings with some space on either side
+  //    so that they can explore the graph to either direction.
+  var user_min = this.taxEngine_.yearlyIndexedEarnings() * 2;
+
+  var computed = Math.max(breakpoint_min, user_min);
+
+  // We would prefer to keep the viewport fixed as the user changes
+  // the benefit, so that it's easier to see what is going on. However
+  // we will adjust if the value gets too close to the edges.
+  if (this.lastRenderedXDollars === -1 ||
+      this.lastRenderedXDollars > computed * 1.3 ||
+      this.lastRenderedXDollars < computed / 1.3)
+   this.lastRenderedXDollars = computed;
+
+  return this.lastRenderedXDollars;
+};
+
+/**
+ * Selects a y-coordinate (in dollars) to be the topmost edge
+ * of our chart.
+ * @return {Number}
+ */
+BreakPointChart.prototype.maxRenderedYDollars = function() {
+  return this.taxEngine_.estimatedFullBenefitForEarnings(
+      this.maxRenderedXDollars());
+};
+
+/**
+ * Utility method which walls lineTo on this.canvas_ after transforming
+ * dollarX and dollarY into canvas x/y coordinates.
+ * @param {Number} dollarX
+ * @param {Number} dollarY
+ */
+BreakPointChart.prototype.lineTo = function(dollarX, dollarY) {
+  this.context_.lineTo(this.canvasX(dollarX), this.canvasY(dollarY));
+};
+
+/**
+ * Utility method which walls moveTo on this.canvas_ after transforming
+ * dollarX and dollarY into canvas x/y coordinates.
+ * @param {Number} dollarX
+ * @param {Number} dollarY
+ */
+BreakPointChart.prototype.moveTo = function(dollarX, dollarY) {
+  this.context_.moveTo(this.canvasX(dollarX), this.canvasY(dollarY));
+};
+
+/**
+ *  Render the bounding box for our chart.
+ */
+BreakPointChart.prototype.renderBoundingBox = function() {
+  this.context_.save();
+  this.context_.lineWidth = 1;
+
+  this.context_.beginPath();
+  this.moveTo(0, 0);
+  this.lineTo(0, this.maxRenderedYDollars());
+  this.lineTo(this.maxRenderedXDollars(), this.maxRenderedYDollars());
+  this.lineTo(this.maxRenderedXDollars(), 0);
+  this.lineTo(0, 0);
+  this.context_.stroke();
+
+  this.context_.restore();
+};
+
+/**
+ *  Render the breakpoint curves. These are constants.
+ */
+BreakPointChart.prototype.renderBreakPoints = function() {
+  // Show the lines between the breakpoints. These are always the same.
+  this.context_.save();
+  this.context_.lineWidth = 4;
+
+  this.context_.beginPath();
+  this.moveTo(0, 0);
+  for (var i = 0; i < BENEFIT_BRACKETS.length; ++i) {
+    var dollarX = BENEFIT_BRACKETS[i].max;
+    if (dollarX === undefined)
+      dollarX = this.maxRenderedXDollars();
+    var dollarY = this.taxEngine_.estimatedFullBenefitForEarnings(dollarX);
+    this.lineTo(dollarX, dollarY);
+  }
+  this.context_.stroke();
+
+  this.context_.restore();
+};
+
+
+/**
+ * Renders a rectangle with three rounded corners.
+ * @param {Number} x canvas x coordinate to draw upper-left corner.
+ * @param {Number} y canvas y coordinate to dray upper-left corner.
+ * @param {Number} width of rectangle
+ * @param {Number} height of rectangle
+ * @param {Number} cornerRadius
+ * @param {Number} squaredCorner (1 = upper left, then clockwise)
+ */
+BreakPointChart.prototype.roundedBox =
+    function(x, y, width, height, cornerRadius, squaredCorner) {
+  this.context_.save();
+  this.context_.beginPath();
+  this.context_.lineCap = 'square';
+
+  if (squaredCorner === 1) {
+    this.context_.moveTo(x, y);
+  } else {
+    this.context_.moveTo(x + cornerRadius, y);
+  }
+  if (squaredCorner === 2) {
+    this.context_.lineTo(x + width, y);
+  } else {
+    this.context_.lineTo(x + width - cornerRadius, y);
+    this.context_.arcTo(x + width, y,
+                        x + width, y + cornerRadius,
+                        cornerRadius);
+  }
+  if (squaredCorner === 3) {
+    this.context_.lineTo(x + width, y + height);
+  } else {
+    this.context_.lineTo(x + width, y + height - cornerRadius);
+    this.context_.arcTo(x + width, y + height,
+                        x + width - cornerRadius, y + height,
+                        cornerRadius);
+  }
+  if (squaredCorner === 4) {
+    this.context_.lineTo(x, y + height);
+  } else {
+    this.context_.lineTo(x + cornerRadius, y + height);
+    this.context_.arcTo(x, y + height,
+                        x, y + height - cornerRadius,
+                        cornerRadius);
+  }
+  if (squaredCorner === 1) {
+    this.context_.lineTo(x, y);
+  } else {
+    this.context_.lineTo(x, y + cornerRadius);
+    this.context_.arcTo(x, y,
+                        x + cornerRadius, y,
+                        cornerRadius);
+  }
+
+  this.context_.fill();
+  this.context_.stroke();
+  this.context_.restore();
+};
+
+/**
+ * Renders a point on the breakpoint curve.
+ * @param {earningsX} earnings dollar value which a point is rendered for.
+ */
+BreakPointChart.prototype.renderEarningsPoint = function(earningsX) {
+  this.context_.save();
+
+  // Where on the breakpoint 'curve' the user's benefit values lie.
+  var userSSA = {
+    x: Math.floor(earningsX),
+    y: Math.floor(this.taxEngine_.estimatedFullBenefitForEarnings(earningsX)),
+  };
+
+  // Add dollar sign and commas for better looking formatting.
+  userSSA.xText = ('$' + userSSA.x).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  userSSA.yText = ('$' + userSSA.y).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
+  // Thin dashed lines out from the user benefit point
+  this.context_.lineWidth = 2;
+  this.context_.lineCap = 'butt';
+  this.context_.setLineDash([3, 5]);
+
+  // Both lines starting at the point and radiating out makes a nifty
+  // animation affect with the dashed lines at the edges of the chart.
+  this.context_.beginPath();
+  this.moveTo(userSSA.x, userSSA.y);
+  this.lineTo(userSSA.x, 0);
+  this.context_.stroke();
+  
+  this.context_.beginPath();
+  this.moveTo(userSSA.x, userSSA.y);
+  this.lineTo(this.maxRenderedXDollars(), userSSA.y);
+  this.context_.stroke();
+
+  // White filled circle with black edge showing the user benefit point.
+  this.context_.setLineDash([]);  // Disable the dashed line from above.
+  this.context_.fillStyle = this.context_.strokeStyle;
+
+  this.context_.beginPath();
+  this.context_.arc(
+      this.canvasX(userSSA.x),
+      this.canvasY(userSSA.y),
+      /*radius=*/ 5,
+      /*startAngle=*/ 0,
+      /*endAngle=*/ 2 * Math.PI);
+  this.context_.fill();
+  this.context_.stroke();
+
+  // Text at the edges showing the actual values, white on colored chip.
+
+  // TODO: Low Priority. Switch which edge of the chip the dotted line
+  // intersects with so as to avoid the two chips ever overlapping.
+
+  // Chip on the bottom edge
+  this.roundedBox(this.canvasX(userSSA.x), this.canvasY(0),
+                  this.context_.measureText(userSSA.xText).width + 6, 19,
+                  5, /*squaredCorner=*/ 1);
+  // Chip on the right edge
+  this.roundedBox(this.canvasX(this.maxRenderedXDollars()) + 1,
+                  this.canvasY(userSSA.y),
+                  this.context_.measureText(userSSA.yText).width + 6, 19,
+                  5, /*squaredCorner=*/ 1);
+
+  this.context_.fillStyle = 'white'
+  this.context_.fillText(  // Text on the bottom edge.
+      userSSA.xText,
+      this.canvasX(userSSA.x) + 2,
+      this.canvasY(0) + 15);
+  this.context_.fillText(  // Text on the right edge.
+      userSSA.yText, 
+      this.canvasX(this.maxRenderedXDollars()) + 3,
+      this.canvasY(userSSA.y) + 15);
+
+  this.context_.restore();
+}
+
+/** Render the breakpoint chart. */
+BreakPointChart.prototype.render = function() {
+  // Canvas tutorial:
+  // http://www.html5canvastutorials.com/tutorials/html5-canvas-element/
+  this.context_.save();
+  this.context_.clearRect(0, 0, this.canvas_.width, this.canvas_.height);
+  
+  this.context_.strokeStyle = '#666';
+  this.renderBoundingBox();
+  this.renderBreakPoints();
+  this.context_.strokeStyle = '#5cb85c';
+
+  this.renderEarningsPoint(this.taxEngine_.yearlyIndexedEarnings());
+
+  this.context_.restore();
+};
+
+/** Toggles on/off functionality of mouseMoveListener. */
+BreakPointChart.prototype.mouseClickListener = function() {
+  var self = this;
+  return function(e) {
+    if (self.mouseToggle === 'ON') {
+      self.mouseToggle = 'OFF'
+    } else {
+      self.mouseToggle = 'ON'
+      // Immediately trigger a rendering based on mouse location.
+      self.mouseMoveListener()(e);
+    }
+  };
+}
+
+/** Renders a 2nd earnings value based on mouse location. */
+BreakPointChart.prototype.mouseMoveListener = function() {
+  var self = this;
+  return function(e) {
+    if (self.mouseToggle == 'OFF')
+      return;
+
+    self.render();
+
+    self.context_.save();
+    self.context_.strokeStyle = '#337ab7';
+    var canvasX = e.clientX - self.canvas_.getBoundingClientRect().left;
+    self.renderEarningsPoint(self.earningsX(canvasX));
+    self.context_.restore();
+  };
+}
---
+++ src/ssa.js
@@ -0,0 +1,253 @@
+var ssaApp = angular.module("ssaApp", ['ngAnimate', 'rzModule']);
+
+var ModeEnum = {
+  INITIAL: 'INITIAL',
+  PASTE_CONFIRMATION: 'PASTE_CONFIRMATION',
+  PASTE_APOLOGY: 'PASTE_APOLOGY',
+  HIDE_MEDICARE: 'HIDE_MEDICARE',
+  RENDER_EARNINGS: 'RENDER_EARNINGS'
+};
+
+ssaApp.controller("SSAController", function ($scope, $filter, $http, $timeout) {
+  $scope.pasteArea = {
+    contents: '',
+    mode: ModeEnum.INITIAL,
+    // Cleaner here than newlines in the actual template.
+    placeholder: '\n\nPaste Result Here'
+  };
+
+  /**
+   * Initialization
+   */
+  $scope.init = function() {
+    $scope.breakPointChart_ = new BreakPointChart();
+    $scope.ageChart_ = new AgeChart();
+    $scope.taxEngine = new TaxEngine();
+    $scope.mode = ModeEnum.INITIAL;
+    $scope.futureYears = [];
+    $scope.futureIsTopValue = false;
+    $scope.all_months = ALL_MONTHS;
+  
+    $scope.showMedicare = true;
+    $scope.showIndexedEarnings = false;
+
+    $scope.breakPointChart_.setTaxEngine($scope.taxEngine);
+    $scope.ageChart_.setTaxEngine($scope.taxEngine);
+    $scope.maybeRenderCharts();
+  };
+
+  $scope.loadDemoData = function(demoId) {
+    // User may have scrolled down. Since we aren't actually chaning URL,
+    // we need to scroll back up to the top for them.
+    window.scrollTo(0, 0);
+
+    if (demoId === 0) {
+      $scope.birth.year = 1950;
+      $scope.birth.maxPossibleYear = 1950;
+      $scope.taxEngine.updateBirthdate($scope.birth);
+      $scope.fetchTestData('averagepaste.txt');
+    }
+    if (demoId === 1) {
+      $scope.birth.year = 1950;
+      $scope.birth.maxPossibleYear = 1950;
+      $scope.taxEngine.updateBirthdate($scope.birth);
+      $scope.fetchTestData('millionpaste.txt');
+    }
+    if (demoId === 2) {
+      $scope.birth.year = 1985;
+      $scope.birth.maxPossibleYear = 1985;
+      $scope.taxEngine.updateBirthdate($scope.birth);
+      $scope.fetchTestData('youngpaste.txt');
+    }
+  }
+
+  // This method exists for now to more quickly play around with our interface.
+  // Rather that relying on copy/paste, it immediately loads an example test
+  // paste via a json request. This method will be deleted at some point.
+  $scope.fetchTestData = function(filename) {
+    $http.get(filename).then(
+      function(contents) {
+        var records = parseYearRecords(contents.data);
+        $scope.taxEngine.initFromEarningsRecords(records);
+        $scope.pasteArea.mode = ModeEnum.RENDER_EARNINGS;
+        $scope.maybeRenderCharts();
+      }, 
+      function(jqxhr, textStatus, error) {
+        console.log("Error loading Test Data");
+        console.log(textStatus);
+        console.log(error);
+      }
+    );
+  };
+
+  $scope.$watch('pasteArea.contents', function(newValue) {
+    /** @type {!Array<!TaxRecord>} */
+    var records = parseYearRecords(newValue);
+    if (records.length === 0)
+      return;
+
+    $scope.taxEngine.initFromEarningsRecords(records);
+    $scope.guessBirthday(records);
+    $scope.pasteArea.mode = ModeEnum.PASTE_CONFIRMATION;
+    $scope.pasteArea.contents = '';
+  });
+
+  $scope.guessBirthday = function(records) {
+    // Estimate a user's birth year, assumming that they first worked at
+    // age 16, the youngest age federally allowed. User can modify this
+    // later to be more accurate.
+    $scope.birth.year = records[0].year - 16;
+    // This is conservative as it assumes the user had wage income the
+    // year they were born. I'm not sure if this is even possible, but
+    // who knows.
+    $scope.birth.maxPossibleYear = records[0].year;
+    $scope.taxEngine.updateBirthdate($scope.birth);
+  }
+
+  $scope.loadReport = function() {
+  }
+
+  $scope.confirmEarningsParse = function(confirmationValue) {
+    if (confirmationValue === 'incorrect') {
+      $scope.pasteArea.mode = ModeEnum.PASTE_APOLOGY;
+    } else if (confirmationValue === 'correct') {
+      $scope.pasteArea.mode = ModeEnum.RENDER_EARNINGS;
+      $scope.maybeRenderCharts();
+    }
+  };
+
+  $scope.showPastePrompt = function() {
+    return $scope.pasteArea.mode === ModeEnum.INITIAL;
+  };
+  $scope.showPasteConfirmation = function() {
+    return $scope.pasteArea.mode === ModeEnum.PASTE_CONFIRMATION;
+  };
+  $scope.showPasteApology = function() {
+    return $scope.pasteArea.mode === ModeEnum.PASTE_APOLOGY;
+  };
+  $scope.showReport = function() {
+    return $scope.pasteArea.mode === ModeEnum.RENDER_EARNINGS;
+  };
+
+  $scope.earningsRecords = function() {
+    return $scope.taxEngine.earningsRecords;
+  };
+
+  /**
+   * Triggers a rendering of both charts iff each chart's initialization
+   * requirements have all been met, otherwise does nothing.
+   */
+  $scope.maybeRenderCharts = function() {
+    if ($scope.breakPointChart_.isInitialized())
+      $scope.breakPointChart_.render();
+    if ($scope.ageChart_.isInitialized())
+      $scope.ageChart_.render();
+  };
+
+  /**
+   * Runs as the various partial html files have loaded. Runs code
+   * specific to initializting them.
+   */
+  $scope.$on("$includeContentLoaded", function(event, templateName) {
+    if (templateName == 'partials/benefit-estimate.html') {
+      $scope.breakPointChart_.setCanvas(
+          document.getElementById('breakpoint-chart-canvas'));
+      $scope.ageChart_.setCanvas(
+          document.getElementById('age-chart-canvas'));
+      $scope.maybeRenderCharts();
+    }
+  });
+
+  $scope.reset = function() {
+    $scope.taxEngine = new TaxEngine();
+    $scope.pasteArea.mode = ModeEnum.INITIAL;
+  };
+
+  $scope.pastYears = function() {
+    var pastYears = [];
+    // We want a multiple of 7 years, want to be sure to include the user's
+    // birth year, and don't want to include more than we need to.
+    
+    // Start at 70 years ago, anyone older than that is already collecting
+    // social security (and can just pick 70 for example).
+    const start = CURRENT_YEAR - 70;
+    // End at the first year we have earnings records for. I don't think anyone
+    // could have earnings records before they were born.
+    var end = $scope.birth.maxPossibleYear;
+    // Add a few years to make sure we have a multiple of 7:
+    end += (7 - (end - start + 1) % 7)
+
+    for (var i = start; i <= end; ++i) {
+      pastYears.push(i);
+    }
+    return pastYears;
+  }
+
+  $scope.updateFutureYears = function(id) {
+    $scope.taxEngine.simulateFutureEarningsYears(
+        /*numYears=*/$scope.futureYearsWorkSlider.minValue,
+        /*wage=*/$scope.futureWageWorkSlider.minValue);
+    $scope.maybeRenderCharts();
+    $scope.futureYears = [];
+    for (var i = 0; i < $scope.futureYearsWorkSlider.minValue; i++) {
+      $scope.futureYears.push(i+CURRENT_YEAR);
+    }
+
+    $scope.futureIsTopValue = ($scope.futureWageWorkSlider.minValue >=
+                               $scope.taxEngine.cutoffIndexedEarnings);
+  }
+ 
+  $scope.futureYearsWorkSlider = {
+    minValue: 0,
+    options: {
+     floor: 0,
+     ceil: 35,
+     step: 1,
+     translate: function(value, sliderId, label) {
+      if (label === 'model') {
+       if (value === 35)
+        return value + '+'
+       return value;
+      }
+      return '';
+     },
+     onChange: $scope.updateFutureYears,
+    },
+
+  };
+
+  $scope.futureWageWorkSlider = {
+    minValue: 1000,
+    options: {
+     floor: 0,
+     ceil: SSA_MULTIPLIERS[SSA_MULTIPLIERS.length - 1].maximumEarnings,
+     step: 1000,
+     translate: function(value, sliderId, label) {
+      if (label === 'model') {
+       if (value === this.options.ceil)
+        return '$' + $filter('number')(value) + '+'
+       return '$' + $filter('number')(value);
+      }
+      return '';
+     },
+     onChange: $scope.updateFutureYears,
+    }
+  };
+
+  $scope.$on("slideEnded", function() {
+    $scope.taxEngine.simulateFutureEarningsYears(
+      /*numYears=*/$scope.futureYearsWorkSlider.minValue,
+      /*wage=*/$scope.futureWageWorkSlider.minValue);
+    $scope.maybeRenderCharts();
+  });
+
+  $scope.birth = {
+    month: "Jan",
+    year: 1982
+  }
+
+  $scope.updateBirthdate = function() {
+    $scope.birth.year = parseInt($scope.birth.year);
+    $scope.taxEngine.updateBirthdate($scope.birth);
+  }
+});
