47b00eec040ffb263e279cd083478eaf79983b2c
Greg Grothaus <ggrothau@gmail.com>
2017-05-21 18:05:58 -0700

    Fix google analytics id.

--- src/analytics.js
+++ src/analytics.js
@@ -1,4 +1,4 @@
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};
-ga('create', 'UA-43468-11', 'auto');
+ga('create', 'UA-243468-11', 'auto');
 ga('set', 'transport', 'beacon');
 ga('send', 'pageview');
