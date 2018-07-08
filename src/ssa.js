7d80ac7bb9c2198f8fc13fc06a18108f1812c3c1
Greg Grothaus <ggrothau@gmail.com>
2018-07-07 20:43:34 -0700

    Offset NRA for day-1 birthdays.

--- src/ssa.js
+++ src/ssa.js
@@ -457,6 +457,10 @@ ssaApp.controller("SSAController", function ($scope, $filter, $http, $timeout) {
     if ($scope.birth.day > 2)
       ssaBirth = ssaBirth.addDuration(new MonthDuration().initFromMonths(1));
     $scope.recipient.updateBirthdate(ssaBirth);
+    // If the birthdate day of month is the 1st, we use the previous month
+    // for calculating the full retirement age.
+    if ($scope.birth.day === 1)
+      $scope.recipient.offsetNra();
 
     // Also set the spouses birthdate to the same, which is a
     // reasonable default to start with, until the user selects
@@ -482,7 +486,12 @@ ssaApp.controller("SSAController", function ($scope, $filter, $http, $timeout) {
     if ($scope.spouseBirth.day > 2)
       ssaBirth = ssaBirth.addDuration(new MonthDuration().initFromMonths(1));
     $scope.spouse.updateBirthdate(ssaBirth);
-    
+    // If the birthdate day of month is the 1st, we use the previous month
+    // for calculating the full retirement age.
+    if ($scope.spouseBirth.day === 1)
+      $scope.recipient.offsetNra();
+
+   
     $scope.higherEarnerSlider.updateBirthDate();
     $scope.lowerEarnerSlider.updateBirthDate();
     $scope.refreshSlider();
