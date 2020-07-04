ef69e388a45b05c53adef114123ae3c533cb163c
Greg Grothaus <ggrothau@gmail.com>
2020-07-04 13:33:48 -0700

    Add tests.

---
+++ spec/tests/recipient_spec.js
@@ -0,0 +1,85 @@
+const Recipient = require('../../src/recipient').Recipient;
+const EarningRecord = require('../../src/recipient').EarningRecord;
+const parsePaste = require('../../src/ssa-parse').parsePaste;
+const Birthdate = require('../../src/birthday').Birthdate;
+
+let earningRecord = new EarningRecord();
+earningRecord.year = 1960;
+earningRecord.taxedEarnings = 19123;
+earningRecord.taxedMedicareEarnings = 19123;
+testEarnings = [];
+testEarnings.push(earningRecord);
+
+describe("Recipient Initialization", function() {
+  let r = new Recipient().initFromEarningsRecords(testEarnings);
+  it ("isInitialized", function() {
+    expect(r.isInitialized()).toBe(true);
+  });
+  it ("numEarningsYears", function() {
+    expect(r.numEarningsYears()).toBe(1);
+  });
+  it ("hasEarningsBefore1978", function() {
+    expect(r.hasEarningsBefore1978).toBe(true);
+  });
+  it ("neededYears", function() {
+    expect(r.neededYears()).toBe(-1);
+  });
+  it ("cutoffIndexedEarnings", function() {
+    // Since the user has <35 years earnings, this should be 0.
+    expect(r.cutoffIndexedEarnings).toBe(0);
+  });
+  it ("credits", function() {
+    expect(r.earnedCredits()).toBe(4);
+    expect(r.plannedCredits()).toBe(0);
+    expect(r.totalCredits()).toBe(4);
+  });
+  it ("primaryInsuranceAmount", function() {
+    // These will need to be updated when there are new wage indices /
+    // index factors.
+    expect(r.totalIndexedEarnings).toBe(62463.77);
+    expect(r.primaryInsuranceAmount()).toBe(133.2);
+    expect(r.primaryInsuranceAmountFloored()).toBe(133);
+  });
+});
+
+describe("Recipient simulateFutureEarnings", function() {
+  let r = new Recipient().initFromEarningsRecords(testEarnings);
+  r.simulateFutureEarningsYears(2, 50000);
+  it ("isInitialized", function() {
+    expect(r.isInitialized()).toBe(true);
+  });
+  it ("numEarningsYears", function() {
+    expect(r.numEarningsYears()).toBe(3);
+  });
+  it ("hasEarningsBefore1978", function() {
+    expect(r.hasEarningsBefore1978).toBe(true);
+  });
+  it ("neededYears", function() {
+    expect(r.neededYears()).toBe(9);
+  });
+  it ("cutoffIndexedEarnings", function() {
+    // Since the user has <35 years earnings, this should be 0.
+    expect(r.cutoffIndexedEarnings).toBe(0);
+  });
+  it ("credits", function() {
+    expect(r.earnedCredits()).toBe(4);
+    expect(r.plannedCredits()).toBe(8);
+    expect(r.totalCredits()).toBe(12);
+  });
+  it ("primaryInsuranceAmount", function() {
+    // These will need to be updated when there are new wage indices /
+    // index factors.
+    expect(r.totalIndexedEarnings).toBe(162463.77);
+    expect(r.primaryInsuranceAmount()).toBe(347.4);
+    expect(r.primaryInsuranceAmountFloored()).toBe(347);
+  });
+  it ("hasFutureEarnings", function() {
+    expect(r.hasFutureEarnings()).toBe(true);
+  });
+  it ("futureEarningsYears", function() {
+    expect(r.futureEarningsYears()).toBe(2);
+  });
+  it ("isEligible", function() {
+    expect(r.isEligible()).toBe(false);
+  });
+});
---
+++ spec/tests/ssa-parse_spec.js
@@ -0,0 +1,259 @@
+const dollarStringToNumber = require('../../src/ssa-parse').dollarStringToNumber;
+const parsePaste = require('../../src/ssa-parse').parsePaste;
+
+describe("dollarStringToNumber", function () {
+  it("Parses Number", function() {
+    expect(dollarStringToNumber("123")).toBe(123);
+  });
+  it("Parses Dollars", function() {
+    expect(dollarStringToNumber("$123")).toBe(123);
+  });
+  it("Parses Dollars with thousands", function() {
+    expect(dollarStringToNumber("$1,234")).toBe(1234);
+  });
+});
+
+// All of the parsePaste tests below have the same data, just in different
+// formats. We use one f'n to test the expected output of all of them.
+function parsePasteExpect(parsed) {
+  expect(parsed.length).toBe(3);
+  expect(parsed[0].year).toBe(2016);
+  expect(parsed[0].taxedEarnings).toBe(80000);
+  expect(parsed[1].year).toBe(2017);
+  expect(parsed[1].taxedEarnings).toBe(90000);
+  expect(parsed[2].year).toBe(2018);
+  expect(parsed[2].taxedEarnings).toBe(100000);
+}
+
+describe("parsePaste", function() {
+  it("Parses Simple Space Delimited Data", function() {
+    let pasteData = "2018 $100,000\n2017 $90,000\n2016 $80,000";
+    parsePasteExpect(parsePaste(pasteData));
+  });
+	it("Parses ssa.tools own formatted table", function() {
+    let pasteData = (
+    		'Year\tAge\tTaxed Earnings\t\tMultiplier\tIndexed Earnings\t\t\n' +
+        '2016\t21\t$80,000\tx\t1.01\t=\t$80,900\tTop 35 Value\n' +
+        '2017\t22\t$90,000\tx\t1.00\t=\t$90,000\t\n' +
+        '2018\t23\t$100,000\tx\t1.00\t=\t$100,000\t\n');
+    parsePasteExpect(parsePaste(pasteData));
+	});
+  it("Parses ssa.gov format (Chrome, Linux, Full Page)", function() {
+    let pasteData = (
+        'KSkip to Content\n' +
+        'my Social Security\n' +
+        'Uncle Sam Sign Out\n' +
+        'My Home  Message Center  Security Settings\n' +
+        'Earnings Record\n' +
+        'Overview\n' +
+        'Estimated Benefits\n' +
+        'Earnings Record\n' +
+        'Replacement Documents\n' +
+        'Social Security Statement\n' +
+        'Your benefits are based on your earnings. If our records are wrong' +
+        'you may not receive all the benefits to which you\'re entitled.\n' +
+        '\n' +
+        'Review your earnings record carefully...\n' +
+        'Limits on taxable earnings for Social Security...\n' +
+        'Contact us about errors...\n' +
+        'Work Year\n' +
+        'Taxed Social Security Earnings\n' +
+        'Taxed Medicare Earnings\n' +
+        '2018\t$100,000\t$100,000\n' +
+        '2017\t$90,000\t$90,000\n' +
+        '2016\t$80,000\t$80,000\n' +
+        'Estimated Total Taxes Paid\n' +
+        'For Social Security\n' +
+        'Paid by you:\n' +
+        '$10,000\n' +
+        'Paid by your employers:\n' +
+        '$10,000\n' +
+        'For Medicare\n' +
+        'Paid by you:\n' +
+        '$10,000\n' +
+        'Paid by your employers:\n' +
+        '$10,000\n' +
+        'Print / Save Your Full Statement\n' +
+        'Get a copy of your Statement information in a convenient, '+
+        'print-friendly format.\n\n' +
+        'Disclaimer\n' +
+        'Download Your Statement Data\n' +
+        'Save your Statement information as an XML file.\n\n' +
+        'How to use this file.\n' +
+        'Privacy PolicyAccessibility Help\n\n');
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Chrome, Linux, Full Table)", function() {
+    let pasteData = (
+        'Work Year\n' +
+        'Taxed Social Security Earnings\n' +
+        'Taxed Medicare Earnings\n' +
+        '2018\t$100,000\t$100,000\n' +
+        '2017\t$90,000\t$90,000\n' +
+        '2016\t$80,000\t$80,000\n')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Chrome, Linux, Table Rows)", function() {
+    let pasteData = (
+        '2018\t$100,000\t$100,000\n' +
+        '2017\t$90,000\t$90,000\n' +
+        '2016\t$80,000\t$80,000\n')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Firefox, Linux, Full Page)", function() {
+    let pasteData = (
+        '\n' +
+        'K\n' +
+        'Skip to Content\n' +
+        'my Social Security\n\n' +
+        '    Gregory A. Grothaus Sign Out \n\n' +
+        '    My Home Message Center Security Settings \n\n' +
+        'Earnings Record\n' +
+        'Overview\n' +
+        'Estimated Benefits\n' +
+        'Earnings Record\n' +
+        'Replacement Documents\n' +
+        'Social Security Statement\n\n' +
+        'Your benefits are based on your earnings. If our records are wrong ' +
+        'you may not receive all the benefits to which you\'re entitled.\n' +
+        'Review your earnings record carefully...\n' +
+        'Limits on taxable earnings for Social Security...\n' +
+        'Contact us about errors...\n' +
+        'Work Year\n\t\n' +
+        'Taxed Social Security Earnings\n\t\n' +
+        'Taxed Medicare Earnings\n' +
+        '2018 \t$100,000 \t$100,000\n' +
+        '2017 \t$90,000 \t$90,000\n' +
+        '2016 \t$80,000 \t$80,000\n' +
+        'Estimated Total Taxes Paid\n' +
+        'For Social Security\n' +
+        'Paid by you:\n' +
+        '$100,082\n' +
+        'Paid by your employers:\n' +
+        '$104,593\n' +
+        'For Medicare\n' +
+        'Paid by you:\n' +
+        '$104,437\n' +
+        'Paid by your employers:\n' +
+        '$104,437\n' +
+        'Print / Save Your Full Statement\n\n' +
+        'Get a copy of your Statement information in a convenient, ' +
+        'print-friendly format.\n' +
+        'Disclaimer\n' +
+        'Download Your Statement Data\n\n' +
+        'Save your Statement information as an XML file.\n' +
+        'How to use this file.\n\n' +
+        '    Privacy PolicyAccessibility Help\n\n\n')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Firefox, Linux, Full Table)", function() {
+    let pasteData = (
+        'Work Year\n\t\n' +
+        'Taxed Social Security Earnings\n\t\n' +
+        'Taxed Medicare Earnings\n' +
+        '2018 \t$100,000 \t$100,000\n' +
+        '2017 \t$90,000 \t$90,000\n' +
+        '2016 \t$80,000 \t$80,000\n')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Firefox, Linux, Table Rows)", function() {
+    let pasteData = (
+        '2018 \t$100,000 \t$100,000\n' +
+        '2017 \t$90,000 \t$90,000\n' +
+        '2016 \t$80,000 \t$80,000\n')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format with trailing whitespace", function() {
+    // User report here indicates that trailing whitespace breaks parsing:
+    // http://www.early-retirement.org/forums/f28/fire-effect-on-ss-benefits-96343-3.html#post2196019
+    let pasteData = (
+        'Work Year \n' +
+        'Taxed Social Security Earnings \n' +
+        'Taxed Medicare Earnings \n' +
+        '2018\t$100,000\t$100,000 \n' +
+        '2017\t$90,000\t$90,000 \n' +
+        '2016\t$80,000\t$80,000 \n')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Edge, Windows, Full Page)", function() {
+    let pasteData = (
+        'K\n' +
+        'Skip to Content\n' +
+        'my Social Security\n' +
+        'Uncle Sam Sign Out \n' +
+        'My Home Message Center Security Settings \n' +
+        'Earnings Record\n' +
+        'Overview\n' +
+        'Estimated Benefits\n' +
+        'Earnings Record\n' +
+        'Replacement Documents\n' +
+        '\n' +
+        'Social Security Statement\n' +
+        'Your benefits are based on your earnings. If our records are wrong ' +
+        'you may not receive all the benefits to which you\'re entitled. \n' +
+        'Review your earnings record carefully... \n' +
+        'Limits on taxable earnings for Social Security... \n' +
+        'Contact us about errors... \n' +
+        'Work Year \n' +
+        'Taxed Social Security Earnings \n' +
+        'Taxed Medicare Earnings \n' +
+        '2018 \n' +
+        '$100,000 \n' +
+        '$100,000 \n' +
+        '2017 \n' +
+        '$90,000 \n' +
+        '$90,000 \n' +
+        '2016 \n' +
+        '$80,000 \n' +
+        '$80,000 \n' +
+        'Estimated Total Taxes Paid\n' +
+        'For Social Security \n' +
+        'Paid by you: \n' +
+        '$000,000 \n' +
+        'Paid by your employers: \n' +
+        '$000,000 \n' +
+        'For Medicare \n' +
+        'Paid by you: \n' +
+        '$000,000 \n' +
+        'Paid by your employers: \n' +
+        '$000,000 \n' +
+        'Print / Save Your Full Statement \n' +
+        'Get a copy of your Statement information in a convenient, ' +
+        'print-friendly format. \n' +
+        'Disclaimer \n' +
+        'Download Your Statement Data \n' +
+        'Save your Statement information as an XML file. \n' +
+        'How to use this file. \n' +
+        'Privacy PolicyAccessibility Help');
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Edge, Windows, Full Table)", function() {
+    let pasteData = (
+        'Work Year \n' +
+        'Taxed Social Security Earnings \n' +
+        'Taxed Medicare Earnings \n' +
+        '2018 \n' +
+        '$100,000 \n' +
+        '$100,000 \n' +
+        '2017 \n' +
+        '$90,000 \n' +
+        '$90,000 \n' +
+        '2016 \n' +
+        '$80,000 \n' +
+        '$80,000')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+  it("Parses ssa.gov format (Edge, Windows, Table Rows)", function() {
+    let pasteData = (
+        '2018 \n' +
+        '$100,000 \n' +
+        '$100,000 \n' +
+        '2017 \n' +
+        '$90,000 \n' +
+        '$90,000 \n' +
+        '2016 \n' +
+        '$80,000 \n' +
+        '$80,000')
+    parsePasteExpect(parsePaste(pasteData));
+  });
+});
