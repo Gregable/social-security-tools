73725401ed5ff6c81bcff48186b1867a4f2f15b6
Greg Grothaus <ggrothau@gmail.com>
2020-12-01 17:58:59 -0800

    Covid (#166)
    * Add a new guide about the effect of Covid on the 1960 born cohort.
    
    * Pick up some files that got left untracked.

---
+++ guide/earnings-cap.mjs
@@ -0,0 +1,24 @@
+import * as constants from "/src/constants.mjs"
+import * as utils from "/src/utils.mjs"
+
+let guidesApp = angular.module("guidesApp", []);
+
+function earningsCaps() {
+  let out = [];
+  for (const year in constants.MAXIMUM_EARNINGS) {
+    out.push({ 'year': year, 'amount': constants.MAXIMUM_EARNINGS[year] });
+  }
+  console.log(out);
+  return out;
+};
+
+guidesApp.controller("EarningsCapController", function ($scope) {
+  /**
+   * Initialization
+   */
+  $scope.init = function() {
+    $scope.earningsCaps = earningsCaps();
+    $scope.MAX_CAP_YEAR = $scope.earningsCaps[$scope.earningsCaps.length - 1].year;
+    $scope.MAX_CAP = $scope.earningsCaps[$scope.earningsCaps.length - 1].amount;
+  };
+});
