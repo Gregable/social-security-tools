7147cf10d83b0fdd3eda19b8b70502a075d0c0ea
Greg Grothaus <ggrothau@gmail.com>
2022-03-23 11:22:40 -0700

    Add Google Analytics reporting for Signed HTTP Exchange Web Vitals data. (#188)
    * Add Google Analytics reporting for Signed HTTP Exchange Web Vitals data.
    
    This doesn't reveal anything about the user, only about the loading
    performance (how fast the page loads).
    
    * Remove extraneous comment line.

--- src/analytics.mjs
+++ src/analytics.mjs
@@ -1,3 +1,32 @@
+ // Web Vitals Module
+import { getCLS, getFID, getLCP } from 'https://unpkg.com/web-vitals?module';
+
+// Web Vitals Data Reporting
+function sendToGoogleAnalytics({name, delta, id}) {
+  // Assumes the global `ga()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/analyticsjs
+  ga('send', 'event', {
+    eventCategory: 'Web Vitals',
+    eventAction: name,
+    // The `id` value will be unique to the current page load. When sending
+    // multiple values from the same page (e.g. for CLS), Google Analytics can
+    // compute a total by grouping on this ID (note: requires `eventLabel` to
+    // be a dimension in your report).
+    eventLabel: id,
+    // Google Analytics metrics must be integers, so the value is rounded.
+    // For CLS the value is first multiplied by 1000 for greater precision
+    // (note: increase the multiplier for greater precision if needed).
+    eventValue: Math.round(name === 'CLS' ? delta * 1000 : delta),
+    // Use a non-interaction event to avoid affecting bounce rate.
+    nonInteraction: true,
+    // Use `sendBeacon()` if the browser supports it.
+    transport: 'beacon',
+
+    dimension1: String(isSXG),
+    dimension2: document.referrer,
+  });
+}
+
 // Only load Google Analytics code if the site is being accessed at
 // https://socialsecurity.tools/ or https://ssa.tools/
 //
@@ -19,7 +48,11 @@ if (document.location.hostname.search('socialsecurity.tools') !== -1 ||
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};
+  window.ga = window.ga || function () { (ga.q = ga.q || []).push(arguments) };
+
+  getCLS(sendToGoogleAnalytics);
+  getFID(sendToGoogleAnalytics);
+  getLCP(sendToGoogleAnalytics);
 } else {
   // Otherwise, register a window.ga dummy function.
   //
