d20492675cd6f2cdb2c72ca47f76208baff8cf69
Greg Grothaus <ggrothau@gmail.com>
2023-07-28 11:02:20 -0700

    Reduce the amount of color.
    With two users, the colors on names gets a little busy.
    
    This change removes the username headers, instead folding them
    into the heading for the "Earnings Record", and removing the color.
    
    This also removes the green color from the big "Eligible" text, though
    the red is still maintained for ineligibile users.
    
    Overall, this makes the page a little less busy, and a little more
    readable.

--- src/components/EarningsReport.svelte
+++ src/components/EarningsReport.svelte
@@ -2,13 +2,14 @@
   import "../global.css";
   import EarningsTable from "./EarningsTable.svelte";
   import FutureEarningsSliders from "./FutureEarningsSliders.svelte";
+  import RecipientName from "./RecipientName.svelte";
   import { Recipient } from "../lib/recipient";
 
   export let recipient: Recipient = new Recipient();
 </script>
 
 <div>
-  <h2>Earnings Record</h2>
+  <h2><RecipientName r={$recipient} apos noColor /> Earnings Record</h2>
 
   <EarningsTable earningsRecords={$recipient.earningsRecords} />
   <FutureEarningsSliders recipient={$recipient} />
