f7611f39efb22870b3095d1f98702aad83f2064e
Greg Grothaus <ggrothau@gmail.com>
2023-08-11 15:18:51 -0700

    Try vercel analytics (#261)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,7 @@
+import {dev} from '$app/environment';
+import {inject} from '@vercel/analytics';
+
 export const prerender = true;
 export const trailingSlash = 'always';
+
+inject({mode: dev ? 'development' : 'production'});
