ff59f0ba8dd2bc6b1a5744ebd09775d94d8bda27
Greg Grothaus <ggrothau@gmail.com>
2023-08-15 09:45:30 -0700

    remove trailing slash

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,5 @@ import {dev} from '$app/environment';
 import {inject} from '@vercel/analytics';
 
 export const prerender = true;
-export const trailingSlash = 'always';
 
 inject({mode: dev ? 'development' : 'production'});
