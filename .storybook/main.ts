1e103f5c1220c25fab81f14582e54caa0526f9ae
Greg Grothaus <ggrothau@gmail.com>
2024-02-20 21:24:51 -0800

    Update Packages, and remove some unused stuff.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,18 @@
 import type { StorybookConfig } from "@storybook/svelte-vite";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   framework: {
     name: "@storybook/sveltekit",
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: "tag"
-  }
+    autodocs: "tag",
+  },
 };
 export default config;
