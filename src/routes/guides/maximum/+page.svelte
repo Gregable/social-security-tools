b5e166ccfcc18f00afef59b35804885349a06bc2
Greg Grothaus <ggrothau@gmail.com>
2025-03-19 17:22:57 -0700

    Update maximum benefit guide. (#386)

--- src/routes/guides/maximum/+page.svelte
+++ src/routes/guides/maximum/+page.svelte
@@ -6,7 +6,7 @@
   const description =
     "What is the maximum monthly social security benefit that someone can reach, and what does it take to reach this maximum?";
   const publishDate = new Date("2019-08-03T00:00:00+00:00");
-  const updateDate = new Date("2023-06-05T00:00:00+00:00");
+  const updateDate = new Date("2025-03-19T00:00:00+00:00");
 
   let schema: GuidesSchema = new GuidesSchema();
   schema.url = "https://ssa.tools/guides/maximum";
@@ -29,10 +29,10 @@
   <p>
     Few people will receive the maximum possible benefit, but how high is the
     maximum? The short answer is that the maximum monthly Social Security
-    benefit that an individual who files a claim in 2023 can receive is:
+    benefit that an individual who files a claim in 2025 can receive is:
   </p>
   <blockquote style="font-size: 18px">
-    <b>$4,555 / month</b>
+    <b>$5,108 / month</b>
   </blockquote>
   <p>In order to reach this high of a benefit, someone must have:</p>
   <ul>
@@ -54,8 +54,8 @@
   <p>At other filing ages, the maximum possible benefit would be smaller:</p>
   <p />
   <ul>
-    <li>$3,627 / month at age 66</li>
-    <li>$2,572 / month at age 62</li>
+    <li>$4,018 / month at age 67</li>
+    <li>$2,831 / month at age 62</li>
   </ul>
   <p>
     The <a href="/">social security calculator</a> will allow you to see the benefit
@@ -65,15 +65,15 @@
   <p>
     The spouse of someone earning the maximum monthly benefit would also be
     eligible to earn a spousal benefit on the same record of as much as half of
-    the primary earner's benefit.
+    the primary earner's benefit at age 67, aka the "primary insurance amount".
   </p>
   <p>
-    In the case of the primary earner earning the maximum benefit of $4,555 /
+    In the case of the primary earner earning the maximum benefit of $5,108 /
     month, the spouse would also be eligible for an additional maximum benefit
-    of $2,277 / month. This would take the total benefit for this family to
-    $6,832 / month. If the spouse also had maxed out his or her own record, the
+    of $2,009 / month. This would take the total benefit for this family to
+    $7,117 / month. If the spouse also had maxed out his or her own record, the
     total benefit for the two maximum personal benefits is simply double a
-    single personal benefit, which is $9,110 / month.
+    single personal benefit, which is $10,216 / month.
   </p>
   <GuideFooter />
 </div>
