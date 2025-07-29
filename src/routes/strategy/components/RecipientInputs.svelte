15fb0c524cbca4842f882f34d371804216938522
Greg Grothaus <ggrothau@gmail.com>
2025-07-29 16:30:36 -0700

    fix bgcolor

--- src/routes/strategy/components/RecipientInputs.svelte
+++ src/routes/strategy/components/RecipientInputs.svelte
@@ -143,6 +143,7 @@
     border: 2px solid #0b0c0c;
     border-radius: 0;
     appearance: none;
+    background-color: white;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='%23000000' d='M10.293 3.293L6 7.586 1.707 3.293A1 1 0 00.293 4.707l5 5a1 1 0 001.414 0l5-5a1 1 0 10-1.414-1.414z'/%3E%3C/svg%3E");
     background-repeat: no-repeat;
     background-position: right 10px center;
