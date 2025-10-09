fe2f5fc580cd2b83d0c67c2f88a4a8429d5be2f5
Greg Grothaus <ggrothau@gmail.com>
2025-10-08 19:45:25 -0700

    Move linopt link slightly

--- src/lib/components/integrations/linopt.com/ReportEnd.svelte
+++ src/lib/components/integrations/linopt.com/ReportEnd.svelte
@@ -364,6 +364,26 @@
       {/if}
     </p>
 
+    <p>
+      <a href="https://linopt.com/" target="_blank" rel="noopener">
+        Visit Linopt
+        <svg
+          class="external-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+          <polyline points="15 3 21 3 21 9" />
+          <line x1="10" y1="14" x2="21" y2="3" />
+        </svg>
+      </a>
+    </p>
+
     <div class="caveats">
       <h3>Caveats</h3>
       <ul>
@@ -409,26 +429,6 @@
         {/if}
       </ul>
     </div>
-
-    <p>
-      <a href="https://linopt.com/" target="_blank" rel="noopener">
-        Visit Linopt
-        <svg
-          class="external-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-          <polyline points="15 3 21 3 21 9" />
-          <line x1="10" y1="14" x2="21" y2="3" />
-        </svg>
-      </a>
-    </p>
   </div>
 </div>
 
