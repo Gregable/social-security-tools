729b300093dee36054762574aa3abaecf22708b7
Greg Grothaus <ggrothau@gmail.com>
2025-10-14 14:56:00 -0700

    Add finpods ai integration

---
+++ src/lib/components/integrations/finpodsai.com/IntroBanner.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import IntegrationBanner from '../IntegrationBanner.svelte';
+  import logo from './favicon.ico';
+</script>
+
+<IntegrationBanner {logo} logoAlt="Fin Pods AI">
+  You're using this calculator via <strong>Fin Pods AI</strong>.
+</IntegrationBanner>
