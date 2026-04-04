163ebd9a567a45ffa26e45291075209e2abbbcdb
Greg Grothaus <ggrothau@gmail.com>
2026-04-04 08:26:12 -0700

    feat: add Rachael Camp Wealth integration (#518)
    Register rachaelcampwealth.com in the integration whitelist with an
    IntroBanner that links back to the site.

---
+++ src/lib/components/integrations/rachaelcampwealth.com/IntroBanner.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import IntegrationBanner from "../IntegrationBanner.svelte";
+  import logo from "./rachel-camp.png";
+
+  export let isReportView: boolean = false;
+</script>
+
+<IntegrationBanner
+  {logo}
+  logoAlt="Rachael Camp Wealth"
+  {isReportView}
+  integrationName="Rachael Camp Wealth"
+  externalLink="https://rachaelcampwealth.com"
+>
+  You're using this calculator via <strong>Rachael Camp Wealth</strong>.
+</IntegrationBanner>
